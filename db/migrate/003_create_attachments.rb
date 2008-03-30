1d1b7d94f38f4f238840a6533d6b233ab8e995f1
Keith Bingman <keith@Calculon.local>
2008-03-30 11:58:11 +0200

    corrected typo

--- db/migrate/003_create_attachments.rb
+++ db/migrate/003_create_attachments.rb
@@ -3,7 +3,7 @@ class CreateAttachments < ActiveRecord::Migration
     create_table :attachments do |t|
       t.column :asset_id,     :integer
       t.column :page_id,      :integer
-      t.columen :position,    :integer
+      t.column :position,    :integer
     end
     
   end
